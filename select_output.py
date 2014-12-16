cd1a0d371660f84b56a8f0e777a99eaf0e4ffb68
rodney <rodney@goshgenetics-X8DA3.(none)>
2014-12-16 18:08:58 +0000

    This script takes a 'Select' statement from the user and outputs the results to a text file

---
+++ select_output.py
@@ -0,0 +1,38 @@
+import MySQLdb as dbm
+import csv
+import sys
+
+db = dbm.connect("localhost","rodney","password","variants" )
+
+cursor = db.cursor()
+
+f = open('variants2.txt','w')
+c = csv.writer(f, delimiter ='\t')
+
+# Take an SQL statement as the first argument.
+sql = sys.argv[1]
+
+try:
+	# Execute the SQL command
+	cursor.execute(sql)
+	# Fetch all the rows in a list of lists.
+	results = cursor.fetchall()
+	desc = cursor.description
+	
+   # Create a header based on the description values of each column
+   # Use index [0] from the description
+	header = []
+	for line in desc:
+		header.append(line[0])
+	print header
+
+	c.writerow(header)
+	for row in results:
+		c.writerow(row)
+		print row
+
+except:
+   print "Error: unable to fetch data"
+   
+
+db.close()
