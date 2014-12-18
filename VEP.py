8e836b9e244bf4d509e842e6f619bd8fdbee26bc
rodney <rodney@goshgenetics-X8DA3.(none)>
2014-12-18 16:32:25 +0000

    This script adds records to the VEPvariants database

--- VEP.py
+++ VEP.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python
-
 import MySQLdb as dbm
 import sys 
-from file import filelist
+from fromFile import filelist
 
 #file = os.path.abspath(sys.argv[1])
 
@@ -11,43 +9,58 @@ record = filelist('vep.txt','record')
 # Remove the header from the dictionary
 record.remove(record[0])
 
-# Open database connection
-db = dbm.connect("localhost","rodney","password","variants" )
 
-# # prepare a cursor object using cursor() method
+db = dbm.connect("localhost","rodney","password","VEPvariants" )
+
 cursor = db.cursor()
 
-# Prepare SQL query to INSERT a record into the database.
+try:
+	for line in record:
+		var = line[0:67]
+		var.remove(var[3])
+		print var
+		print len(var)
+		# For floats use the %f placeholder instead of the %d placeholder
+		cursor.executemany("""insert ignore into Variants (cDNA, 
+					Refseq, HGVSNomen, GT, GQ, SDP, DP, RD,
+					AD, FREQ, PVAL, RBQ, ABQ, RDF, RDR, ADF,
+					ADR, CHROM, POS, REF, ALT, ID, info_ADP,
+					info_WT, info_HET, info_HOM, info_NC, CSQ_Allele,	
+					CSQ_Gene, CSQ_Feature, CSQ_Feature_type, CSQ_Consequence,
+					CSQ_cDNA_position, CSQ_CDS_position, CSQ_Protein_position,
+					CSQ_Amino_acids, CSQ_Codons, CSQ_Existing_variation,	
+					CSQ_AA_MAF, CSQ_EA_MAF, CSQ_ALLELE_NUM, CSQ_EXON, CSQ_INTRON,
+					CSQ_DISTANCE, CSQ_CLIN_SIG, CSQ_CANONICAL, CSQ_SYMBOL,
+					CSQ_BIOTYPE, CSQ_ENSP, CSQ_DOMAINS, CSQ_CCDS, CSQ_AFR_MAF, CSQ_AMR_MAF,
+					CSQ_ASN_MAF, CSQ_EUR_MAF, CSQ_FATHMM, SIFT, SIFT_scoe, POLYPHEN, POLYPHEN_SCORE,	
+					1KG_Allele,	1KG_MAF, HGVS_transcript, HGVSc, HGVS_protein,	HGVSp) 
+					values (%s,%s,%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,
+					%s,%s,%s,%s,%s,%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,
+					%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, 
+					%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s,%s,%s,%s,%s,%s, 
+					%s, %s,%s,%s,%s,%s)""" , [var])
+		#print "Rows in the Variants table were populated"
+except:
+	print "an error occurred inserting variants"
+	db.rollback()
 
-
-for line in record:
-	PK = line[0]
-	refseq = line[1]
-	hgvsnomen = line[2]
-	effect = line[32]
-	dbsnp = line[38]
-	maf = float(line[62])
-	espaa = float(line[39])
-	espea = float(line[40])
-	sift = line[57]
-	siftscore = float(line[58])
-	polyphen = line[59]
-	polyphenscore = float(line[60])
-	protein = line[66]
-	proteindom = line[50]
 	
-	cursor.execute("insert ignore into Variants (cDNA, Refseq, \
-		  HGVSNomen, Effect, dbSNP, MAF, ESP_AA, ESP_EA, SIFT, SIFTweight, \
-		  Polyphen, PolyphenScore, Protein, ProteinDomain) \
-		  values ('%s', '%s', '%s', '%s', '%s', '%d', '%d', '%d', '%s', '%d' \
-		  '%s', '%d', '%s', '%s')" % \
-		  (PK, refseq, hgvsnomen, effect, dbsnp, maf, espaa \
-		  , espea, sift, siftscore, polyphen, polyphenscore \
-		  , protein, proteindom))
-   # Commit your changes in the database
-	db.commit()
-print "records successfully added to database"
+try:
+	for line in record:
+		PK = line[0]
+		sample = line[3]
+		gt = line[4]
+		cursor.execute("insert ignore into Samples ( SampleNumber)\
+					  values ('%s')" % (sample))
+		#print "Rows in the Samples table were populated"
+		
+		cursor.execute("insert into Occurrence (SampleNumber, cDNA, Genotype) \
+					   values ('%s', '%s', '%s')" % (sample, PK, gt))
+		#print "Rows in the Occurrence table were populated" 
+		db.commit()
+	print "records successfully added to database"
+except:
+	print "an error occured"
+	db.rollback()
 
-
-# disconnect from server
 db.close()
