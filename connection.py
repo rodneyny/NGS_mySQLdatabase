bab435a997538342258d9826ff29a883aaa280fd
Rodney Nyanhete <rodney.nyanhete@postgrad.manchester.ac.uk>
2015-01-08 17:35:36 +0000

    Started the connect function

--- connection.py
+++ connection.py
@@ -1,4 +1,8 @@
 import MySQLdb as dbm
 
+def connect():
+	# Open database connection
+	db = MySQLdb.connect("localhost","rodney","password","VEPvariants" )
 
-def connect
+	# prepare a cursor object using cursor() method
+	cursor = db.cursor()
