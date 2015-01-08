b35d6f9b3d93cb864915357ad1069ab585968153
Rodney Nyanhete <rodney.nyanhete@postgrad.manchester.ac.uk>
2015-01-08 12:06:23 +0000

    The archive folder contains scripts and files which are not required for the database. Most of these scripts are just examples of how carry out certain tasks

---
+++ archive/sqltest.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+import MySQLdb as dbm
+import sys 
+from fromFile import filelist
+
+#file = os.path.abspath(sys.argv[1])
+
+record = filelist('vep.txt','record')
+
+# Remove the header from the dictionary
+record.remove(record[0])
+
+# Open database connection
+db = dbm.connect("localhost","rodney","password","VEPvariants" )
+
+# # prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to INSERT a record into the database.
+
+try:
+	for line in record:
+		var = line[0:67]
+		var.remove(var[3])
+
+		# For floats use the %f placeholder instead of the %d placeholder
+		cursor.executemany("""insert ignore into Variants (cDNA, 
+					Refseq, HGVSNomen, GT, GQ, SDP, DP, RD,
+					AD, FREQ, PVAL, RBQ, ABQ, RDF, RDR, ADF,
+					ADR, CHROM, POS, REF, ALT, ID, info_ADP,
+					info_WT, info_HET, info_HOM, info_NC, CSQ_Allele,	
+					CSQ_Gene, CSQ_Feature, CSQ_Feature_type, CSQ_Consequence,
+					CSQ_cDNA_position, CSQ_CDS_position, CSQ_Protein_position,
+					CSQ_Amino_acids, CSQ_Codons, CSQ_Existing_variation,	
+					CSQ_AA_MAF, CSQ_EA_MAF, CSQ_ALLELE_NUM, CSQ_EXON, CSQ_INTRON,
+					CSQ_DISTANCE, CSQ_CLIN_SIG, CSQ_CANONICAL, CSQ_SYMBOL,
+					CSQ_BIOTYPE, CSQ_ENSP, CSQ_DOMAINS, CSQ_CCDS, CSQ_AFR_MAF, CSQ_AMR_MAF,
+					CSQ_ASN_MAF, CSQ_EUR_MAF, CSQ_FATHMM, SIFT, SIFT_scoe, POLYPHEN, POLYPHEN_SCORE,	
+					1KG_Allele,	1KG_MAF, HGVS_transcript, HGVSc, HGVS_protein,	HGVSp) 
+					values (%s,%s,%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,
+					%s,%s,%s,%s,%s,%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,
+					%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, 
+					%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s,%s,%s,%s,%s,%s, 
+					%s, %s,%s,%s,%s,%s)""" , [var])
+		#print "Rows in the Variants table were populated"
+except:
+	print "an error occurred inserting variants"
+	db.rollback()
+
+	
+try:
+	for line in record:
+		PK = line[0]
+		sample = line[3]
+		gt = line[4]
+		cursor.execute("insert ignore into Samples ( SampleNumber)\
+					  values ('%s')" % (sample))
+		#print "Rows in the Samples table were populated"
+		
+		cursor.execute("insert ignore into Occurrence (SampleNumber, cDNA, Genotype) \
+					   values ('%s', '%s', '%s')" % (sample, PK, gt))
+		#print "Rows in the Occurrence table were populated" 
+		db.commit()
+	print "records successfully added to database"
+except:
+	print "an error occurred inserting samples"
+	db.rollback()
+	print "Rows in the Variants table were populated"
+	db.commit()
+	
+cursor.execute("select cDNA ,count(SampleNumber)as EpisodeCount \
+from Occurrence \
+group by cDNA")	
+
+samcount = cursor.fetchall()
+
+for line in samcount:
+	cdna = line[0]
+	count = line[1]
+	print cdna
+	cursor.execute("Update Variants set Frequency \
+					= %s \
+					where cDNA = %s" ,(count,cdna))
+	db.commit()
+
+
+# disconnect from server
+db.close()
