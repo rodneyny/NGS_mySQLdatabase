26a3a8ff7f56b178d7c094b45bbbaff2e14dc597
Rodney Nyanhete <rodney.nyanhete@postgrad.manchester.ac.uk>
2015-01-12 17:13:47 +0000

    Connect function for connecting to the VEPvariants database with a password

--- connection.py
+++ connection.py
@@ -1,8 +1,18 @@
 import MySQLdb as dbm
+import getpass
 
 def connect():
+	# fetches username of person logged into the system
+	# this username must match the username in the mysql user table 
+	userid = getpass.getuser()
+	
+	# mysql password for user 
+	upass = getpass.getpass('Please enter your password: ')
+
 	# Open database connection
-	db = MySQLdb.connect("localhost","rodney","password","VEPvariants" )
+	data = dbm.connect("localhost", userid, upass,"VEPvariants" )
 
 	# prepare a cursor object using cursor() method
-	cursor = db.cursor()
+	cur = data.cursor()
+	
+	return cur, data
